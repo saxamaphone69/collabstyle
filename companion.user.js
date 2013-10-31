871f80d2219379aa7517e1dba548e019e01e4d96
Johnny <johnny.karanikolas@gmail.com>
2013-10-30 17:47:16 -0700

    cleaning up and what have you

--- companion.user.js
+++ companion.user.js
@@ -1,156 +1,109 @@
 // ==UserScript==
-// @name          4chan s
+// @name          4chan sidedish
 // @author        saxamaphone69
-// @version       0.4
+// @version       0.5
 // @grant         none
-// @run-at        document-end
 // @include       *://boards.4chan.org/*
 // ==/UserScript==
 
 (function () {
+    'use strict';
     var d = document;
-    function bigBad() {
+    function init() {
         /*
         function scrollHeaderCheck() {
-            var lst = 0, st, direction;
-			function detectDirection() {
-				st = window.pageYOffset;
-				if (st > lst) {
-					direction = 'down';
-					document.documentElement.classList.remove('up');
-					document.documentElement.classList.add('down');
-				} else {
-					direction = 'up';
-					document.documentElement.classList.remove('down');
-					document.documentElement.classList.add('up');
-				}
-
-				lst = st;
-
-				return direction;
-
-				}
-
-				window.onscroll = function scroll() {
-					var dir = detectDirection();
-					console.log(dir);
-					//document.documentElement.classList.add(dir);
-				}
-        };
-        scrollHeaderCheck();
-        */
-        // create a button to remove all the <style> elements
-        /*
-        var rmStyleButton = d.createElement('button');
-        rmStyleButton.id = 'removestyle';
-        rmStyleButton.innerHTML = 'remove styles';
-        d.body.appendChild(rmStyleButton);
-        d.getElementById('removestyle').onclick = function () {
-        */
-            var rmStyles = d.querySelectorAll('style');
-            for (var i = 0, j = rmStyles.length; i < j; i++) {
-                rmStyles[i].remove();
-            }
-        // };
-        // ensure the first navlink stays so i can style the catalog button
+		var lst = 0, st, direction;
+		function detectDirection() {
+		st = window.pageYOffset;
+		if (st > lst) {
+		direction = 'down';
+		d.documentElement.classList.remove('up');
+		d.documentElement.classList.add('down');
+		} else {
+		direction = 'up';
+		d.documentElement.classList.remove('down');
+		d.documentElement.classList.add('up');
+		}
+		lst = st;
+		return direction;
+		}
+		window.onscroll = function scroll() {
+		var dir = detectDirection();
+		console.log(dir);
+		d.documentElement.classList.add(dir);
+		}
+		};
+		scrollHeaderCheck();
+		*/
+        // Remove all the <style> elements from the document.
+        var rmStyles = d.querySelectorAll('style');
+        for (var i = 0, j = rmStyles.length; i < j; i++) {
+            rmStyles[i].remove();
+        }
+        // Ensure the first .navLink stays so I can style the Catalog link.
         d.querySelector('.navLinks a[href$="catalog"]').classList.add('catalog-link');
         d.querySelector('.navLinks a[href$="catalog"]').innerHTML = '';
-        // wrap the numbers in the .summary with a span
-        // TODO: if the thread is expanded, it needs to rewrap
+        // Wrap the numbers in the .summary with a <span> tag.
         var stylin = d.querySelectorAll('.summary');
         for (var i = 0, j = stylin.length; i < j; i++) {
             var styleContent = stylin[i].textContent;
             var newContent = styleContent.replace(/(\d+)/g, '<span>$1</span>');
             stylin[i].innerHTML = newContent;
         }
-        // add a class to <html> depending if we're on the index or in a thread
+        // Add a class to <html>, depending if we're on the index or not.
         if (location.pathname.split('/')[2] != 'res') {
             d.documentElement.classList.add('on-index');
         } else {
             d.documentElement.classList.add('in-thread');
         }
-        // add an image opacity toggler
+        // Add an image opacity toggler.
         var parentalMode = d.createElement('div');
         parentalMode.id = 'parental-mode';
         d.body.appendChild(parentalMode);
         document.getElementById('parental-mode').onclick = function () {
             d.documentElement.classList.toggle('parental-mode');
         };
-        // add a dark as my soul toggler
+        // Add a 'Dark As My Soul' toggler.
         var dams = d.createElement('div');
         dams.id = 'dams';
         d.body.appendChild(dams);
         document.getElementById('dams').onclick = function () {
             d.documentElement.classList.toggle('dams');
         };
-        // add a 4chan X settings button
+        // Add a button to open the 4chan X settings.
         var chanxsettings = d.createElement('div');
         chanxsettings.id = 'chanxsettings';
         d.body.appendChild(chanxsettings);
         document.getElementById('chanxsettings').onclick = function () {
             d.dispatchEvent(new CustomEvent('OpenSettings'));
         };
-        // change [Reply] to View for styling
+        // Change [Reply] to View for styling purposes.
         var replytoview = d.querySelectorAll('.postNum > span');
-        for (var i = 0, j =  replytoview.length; i < j; i++) {
+        for (var i = 0, j = replytoview.length; i < j; i++) {
             var replytoviewlink = replytoview[i].querySelector('a').href;
             replytoview[i].innerHTML = '<a class="view-thread" href="' + replytoviewlink + '">View</a>';
         }
-        // this creates a <style> element to edit instead of using stylish
-        /*
-        var head = d.getElementsByTagName('head')[0];
-        var s = d.createElement('style');
-        s.setAttribute('type', 'text/css');
-        s.setAttribute('contenteditable', 'true');
-        s.setAttribute('id', 'editme');
-        head.appendChild(s);
-        $.addStyle = function(css) {
-    var style;
-    style = $.el('style', {
-      textContent: css
-    });
-    $.asap((function() {
-      return d.head;
-    }), function() {
-      return $.add(d.head, style);
-    });
-    return style;
-  };
-        */
-        /*
-        function addStyle() {
-           var head = d.getElementsByTagName('head')[0];
-           var s = d.createElement('style');
-           var css = "";
-           s.textContent = css;
-           head.appendChild(s);
-        };
-        var addStyleButton = d.createElement('button');
-        addStyleButton.id = 'addstyle';
-        addStyleButton.innerHTML = 'add styles';
-        d.body.appendChild(addStyleButton);
-        d.getElementById('addstyle').onclick = function () {
-            addStyle();
-        };*/
-        d.onkeyup = function(e) {
-           var tag = e.target.tagName.toLowerCase();
-           if ( e.which === 190 && tag != 'input' && tag != 'textarea')  {
-               if (d.documentElement.classList.contains('sidebar-inactive')) {
-                d.documentElement.classList.remove('sidebar-inactive');
-                d.documentElement.classList.add('sidebar-active');
-            } else {
-                d.documentElement.classList.remove('sidebar-active');
-                d.documentElement.classList.add('sidebar-inactive');
+        // Upon pressing '.', the sidebar will activate and automatically focus the QR.
+        d.onkeyup = function (e) {
+            var tag = e.target.tagName.toLowerCase();
+            if (e.which === 190 && tag != 'input' && tag != 'textarea') {
+                if (d.documentElement.classList.contains('sidebar-inactive')) {
+                    d.documentElement.classList.remove('sidebar-inactive');
+                    d.documentElement.classList.add('sidebar-active');
+                } else {
+                    d.documentElement.classList.remove('sidebar-active');
+                    d.documentElement.classList.add('sidebar-inactive');
+                }
+                var leQR = d.querySelector('html.sidebar-active #qr');
+                leQR.querySelector('textarea').focus();
             }
-               var leQR = d.querySelector('html.sidebar-active #qr');
-               leQR.querySelector('textarea').focus();
-           }
         };
-        // add a sidebar toggler
+        // Add a sidebar toggler.
         var sidebarToggle = d.createElement('div');
         sidebarToggle.id = 'sidebar-toggle';
         d.body.appendChild(sidebarToggle);
         d.documentElement.classList.add('sidebar-inactive');
+        // Create a sidebar.
         var sidebar = d.createElement('div');
         sidebar.id = 'sidebar';
         d.body.appendChild(sidebar);
@@ -163,15 +116,18 @@
                 d.documentElement.classList.add('sidebar-inactive');
             }
         };
+        // Get that newly created sidebar...
         var Ssidebar = d.getElementById('sidebar');
         function checkNadd(el) {
             if (el) {
                 Ssidebar.appendChild(el);
-            } else {
-                console.log('woah there, couldn\'t find ' + el + ', sorry about that :(');
-            }
+            } /*else {
+		        setTimeout(function() {
+		        Ssidebar.appendChild(el);
+		    }, 100);
+		};*/
         }
-        checkNadd(d.getElementById('header'));
+        // then add everything to it. There must be a better way to do this....
         checkNadd(d.getElementById('qr'));
         checkNadd(d.getElementById('updater'));
         checkNadd(d.getElementById('thread-stats'));
@@ -182,6 +138,8 @@
         checkNadd(d.getElementById('chanxsettings'));
         checkNadd(d.getElementById('parental-mode'));
         checkNadd(d.getElementById('header'));
+        // Seriously that's fucking gross.
     }
-    d.addEventListener('DOMContentLoaded', bigBad, false);
+    d.addEventListener('DOMContentLoaded', init, false);
 }());
+// document.addEventListener('4chanXInitFinished', function(){ alert('we loaded, now') }, false);
